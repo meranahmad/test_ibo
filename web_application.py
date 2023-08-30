2df25113b0738f0dc850fa89dd4f0f2828d872d3
Meran Ahmad <meran@live.com>
2023-08-30 20:41:07 +0100

    Update web_application.py
    I done this from the web page

--- web_application.py
+++ web_application.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("<h1>Hello, World! meran test done</h1>")
+        self.write("<h1>Hello, World! meran test done. edited at git hub</h1>")
 
 def make_app():
     return tornado.web.Application([
