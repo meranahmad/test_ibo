cd5173b57ac0f8c4c4f657bd8b61b083f84308e8
meran <meran.ahmad@halliburton.com>
2023-08-30 20:33:54 +0100

    test is done

--- web_application.py
+++ web_application.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("<h1>Hello, World! meran test</h1>")
+        self.write("<h1>Hello, World! meran test done</h1>")
 
 def make_app():
     return tornado.web.Application([
