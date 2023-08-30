db2456773364cfab4df97c3edc700f7e6108e879
meran <meran.ahmad@halliburton.com>
2023-08-30 20:23:21 +0100

    added web application first commit

---
+++ web_application.py
@@ -0,0 +1,18 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("<h1>Hello, World!</h1>")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8080)
+    print("Server started at http://localhost:8080/")
+    tornado.ioloop.IOLoop.current().start()
+
